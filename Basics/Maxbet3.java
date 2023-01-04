39656c25c2519041ecddd14819f19a3c15d7b964
GawaliAbhishek <gawalibhshk@gmail.com>
2023-01-04 10:52:51 +0530

    First Day

---
+++ Basics/Maxbet3.java
@@ -0,0 +1,17 @@
+public class Maxbet3 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=8;
+        int c=4;
+      if(a>b && a>c){
+        System.out.println(a+" is Greater");
+      }
+      else if(b>a && b>c){
+        System.out.println(b+" is Greater");
+      }
+      else{
+        System.out.println(c+" is the Greatest");
+      }
+           
+    }
+}
---
+++ Basics/Palindrome.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number");
+            int num =sc.nextInt();
+            System.out.println("Number of digits in number is "+numofDigiit(num));
+            System.out.println("Number of digits in number is "+shortrickfordigitinnumber(num));
+
+           sumandproductofdigits(num);
+           sumofevenandproductofeven(num);
+            int rev=0;
+            int temp=num;
+      while(num>0){
+            rev=(rev*10)+num%10;
+            num=num/10;
+      }
+      if(rev==temp){
+            System.out.println("Palindrome");
+      }
+      else
+      System.out.println("Not Palindrome");
+        }
+        
+    }
+
+
+
+    public static int numofDigiit(int num){
+        int count=0;
+        while(num>0){
+      
+            num=num/10;
+            count++;
+        }
+        return count;
+    }
+
+    public static void sumandproductofdigits(int num){
+        int sum=0;
+        int pro=1;
+        while(num>0){
+            int digit=num%10;
+            sum=sum+digit;
+            pro=pro*digit;
+            num=num/10;
+        }
+
+        System.out.println("Sum is "+sum);
+        System.out.println("Product is "+pro);
+
+    }
+
+
+    public static void sumofevenandproductofeven(int num){
+        
+        int sum=0;
+        int pro=1;
+        while(num>0){
+            int digit=num%10;
+            if(digit%2==0){
+                sum=sum+digit;
+            }
+            else{
+                pro=pro*digit;
+            }
+            num=num/10;
+        }
+
+        System.out.println("Sum of Even Number is "+sum);
+        System.out.println("Product of Odd Number is "+pro);
+    }
+
+
+    public static int shortrickfordigitinnumber(int num){
+        return (int) (Math.log10(num)) +1;
+    }
+}
