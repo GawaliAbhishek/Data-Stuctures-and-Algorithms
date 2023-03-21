22de048f7bafb02c090802e16d640f490ed75b38
GawaliAbhishek <gawalibhshk@gmail.com>
2023-03-21 22:15:40 +0530

    Heap Done

--- Heap/Opretions.java
+++ Heap/Opretions.java
@@ -1,42 +1,81 @@
 
-import java.util.ArrayList;
+    import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Opretions {
-    
+
     public static void main(String[] args) {
-      
-        List<Integer> list =new ArrayList<>();
+//-1,40,10,30,50,60,15
+        List<Integer> list = new ArrayList<>();
         list.add(-1);
-        list.add(50);
         list.add(40);
-        list.add(45);
-        list.add(30);
-        list.add(20);
-        list.add(35);
         list.add(10);
-        insert(list, 60);;
+        list.add(30);
+        list.add(50);
+        list.add(60);
+        list.add(15);
+        
+      
+        for(int i =(list.size()-1)/2;i>0;i--){
+            heapify(list, i);
+        }
+        delete(list);
+       
         System.out.println(list);
     }
 
-    public static void insert(List<Integer> arr,int n){
+    public static void insert(List<Integer> arr, int n) {
         arr.add(n);
-        int i=arr.size()-1;
-        
-        while(i> 1){
-            int p=i/2;
-            if(arr.get(p)<arr.get(i)){
-                int temp=arr.get(i);
-                arr.set(i,arr.get(p));
-                arr.set(p,temp);
-                i=p;
-            }
-            else{
+        int i = arr.size() - 1;
+
+        while (i > 1) {
+            int p = i / 2;
+            if (arr.get(p) < arr.get(i)) {
+                int temp = arr.get(i);
+                arr.set(i, arr.get(p));
+                arr.set(p, temp);
+                i = p;
+            } else {
                 return;
             }
         }
     }
 
 
-   
+    private static void heapify(List<Integer> list,int i){
+        int largest=i;
+        int leftChild=i*2;
+        int rightChild=i*2+1;
+
+        if(leftChild<=list.size()-1)
+        if(list.get(largest)<list.get(leftChild))
+        largest=leftChild;
+
+        if(rightChild<=list.size()-1)
+        if(list.get(largest)<list.get(rightChild))
+        largest=rightChild;
+
+        if(largest!=i){
+            int temp=list.get(i);
+            list.set(i,list.get(largest));
+            list.set(largest,temp);
+            heapify(list, largest);
+        }
+
+        
+    }
+
+
+    public static void delete(List<Integer> list){
+        list.remove(1);
+        list.set(1,list.get(list.size()-1));
+
+        for(int i=(list.size()-1)/2;i>0;i--){
+            heapify(list, i);
+        }
+    }
+
+
+
 }
---
+++ Sorting/HeapSort.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class HeapSort {
+    
+    public static void main(String[] args) {
+        int arr[]={-1,5,4,3,2,1};
+        for(int i=(arr.length-1)/2;i>0;i--)
+        hepify(arr,arr.length-1, i);
+
+        Sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    public static void Sort(int arr[]){
+
+        for(int i=arr.length-1;i>0;i--){
+            int temp=arr[1];
+            arr[1]=arr[i];
+            arr[i]=temp;
+            hepify(arr,i-1, 1);
+        }
+    }
+
+    private static void hepify(int arr[],int n,int i){
+        int largest=i;
+        int left=2*i;
+        int right=2*i+1;
+
+        if(left<=n)
+        if(arr[left]>arr[largest])
+        largest=left;
+
+        if(right<=n)
+        if(arr[right]>arr[largest])
+        largest=right;
+
+        if(largest!=i){
+            int temp=arr[largest];
+            arr[largest]=arr[i];
+            arr[i]=temp;
+            hepify(arr,n, largest);
+        }
+    }
+}
